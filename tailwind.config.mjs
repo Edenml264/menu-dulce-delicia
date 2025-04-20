b9d38b59396613a350a993c37068111f37fcd639
Imac102050 <imac102050@iMac-de-Imac102050.local>
2025-04-20 03:06:05 -0700

    fix: configuración oficial Tailwind Astro (@astrojs/tailwind)

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+import { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
